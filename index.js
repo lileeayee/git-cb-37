9f9a58c359091c05daad5993a3da42a76e088811
lileeayee <lileeayee@gmail.com>
2020-02-07 20:08:25 -0600

    Commit 1

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hola");
+//si el archivo esta en rojo, quiere decir que estamos en :Working Directory: y el archivo no esta guardado aun +git status+
+//Staging area: +git add+ git add index.js
